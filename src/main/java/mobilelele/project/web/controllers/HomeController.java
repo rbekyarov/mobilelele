bfc9bfce857958f5cd66384fdf2d600fd4e3ee32
Radoslav Bekyarov <rbekyarov82@gmail.com>
2023-01-20 09:39:15 +0200

    up

--- src/main/java/mobilelele/project/web/controllers/HomeController.java
+++ src/main/java/mobilelele/project/web/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package mobilelele.project.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController extends BaseController{
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
 }
--- src/main/java/mobilelele/project/web/controllers/UserController.java
+++ src/main/java/mobilelele/project/web/controllers/UserController.java
@@ -6,4 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/users")
 public class UserController extends BaseController{
+
+
 }
